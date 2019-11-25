7c4bf7d2236889633c53365ad4c6a693460af91d
Ryan Ziegenfus <thewebcoderz@gmail.com>
2019-11-25 09:53:06 -0500

    added cors

--- api/server.js
+++ api/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const server = express();
+const cors = require('cors');
 const users_router = require('./routers/users_router');
 
+server.use(cors());
 server.use('/', users_router);
 
 module.exports = server
